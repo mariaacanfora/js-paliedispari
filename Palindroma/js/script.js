2fda3f275bd9bf49055d14f537047419ddadbc27
mariaacanfora <ma.maria.acanfora@gmail.com>
2021-10-18 15:47:45 +0200

    Bonus completed

--- Palindroma/js/script.js
+++ Palindroma/js/script.js
@@ -1,29 +1,61 @@
 //chiedo parola all'utente
-let word = prompt("Inserisci una parola");
+const checkBtn = document.getElementById("check-btn");
 
-console.log("La parola ha: " + word.length + " lettere");
+let userWord;
+checkBtn.addEventListener("click", function(){
 
-function isPalindroma(word) {
+    
+    userWord = document.getElementById("user-word").value;
+    console.log(userWord);
+    console.log("La parola ha: " + userWord.length + " lettere");
+
+    const wordPalindrome = isPalindrome(userWord);
+    let msgPalindrome;
+    let textColorClass;
+    if (wordPalindrome){
+        msgPalindrome = "è palindroma!";
+        textColorClass = "text-success"
+    } else{
+        msgPalindrome = "non è palindroma!";
+        textColorClass = "text-danger"
+    }
+    
+    const main = document.querySelector("main");
+    console.log(main);
+    
+    const textPalindrome = document.createElement("h2");
+    textPalindrome.classList.add("text-center");
+
+
+    textPalindrome.innerHTML = `La parola <span class="fst-italic">${userWord}</span> <span class="${textColorClass}">${msgPalindrome}</span>`;    
+    main.append(textPalindrome); 
+})
+
+
+/**
+ *
+ * @param {string} word 
+ * @returns 
+ */
+function isPalindrome(word) {
     //estraggo le singole lettere della parola e le inserisco in un array
     splitWord = word.split('');
-    console.log(splitWord);
+    //console.log(splitWord);
 
     //inverto l'ordine degli elementi nell'array
     reverseWord = splitWord.reverse();
-    console.log(reverseWord);
+    //console.log(reverseWord);
 
     //creo e restituisco una nuova stringa concatenando tutti gli elementi nell'array 
     joinWord = reverseWord.join('');
-    console.log(joinWord);
+    //console.log(joinWord);
 
     //controllo se la parola al contrario è uguale alla parola inizialmente inserita
-    let palindroma = true; 
+    let palindrome = true; 
 
     if (joinWord != word) {
-        palindroma = false;
+        palindrome = false;
     } 
 
-    return palindroma;
+    return palindrome;
 }
-
-alert(`La parola ${word} è palindroma? ${word=isPalindroma(word)}`);
